31723e354f602e23491fc3bb3ebc99f771037afe
Bartek0074 <bartekdo123@gmail.com>
2023-09-01 21:32:05 +0200

    improved initBackground

--- src/classes/Star.js
+++ src/classes/Star.js
@@ -1,19 +1,37 @@
 import { CANVAS_HEIGHT } from '../data/variables';
 export default class Star {
-	constructor(x, y, radius, velocity) {
+	constructor(x, y, radius, color, alpha) {
 		this.position = {
 			x: x,
 			y: y,
 		};
 
-		this.velocity = velocity;
-
 		this.radius = radius;
+
+		this.color = color;
+		this.alpha = alpha;
+
+		const rgbValues = color.match(/\d+/g);
+
+		const R = parseInt(rgbValues[0]);
+		const G = parseInt(rgbValues[1]);
+		const B = parseInt(rgbValues[2]);
+
+		this.colorAlpha = `rgba(${R}, ${G}, ${B}, ${alpha})`;
+
+		this.velocity = (this.alpha * Math.floor(Math.random() * 10 + 2)) / 20;
 
 		this.draw = function (ctx) {
 			ctx.beginPath();
-			ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
-			ctx.fillStyle = '#FCFFE755';
+			ctx.arc(
+				this.position.x,
+				this.position.y,
+				this.radius,
+				0,
+				2 * Math.PI,
+				false
+			);
+			ctx.fillStyle = this.colorAlpha;
 			ctx.fill();
 			ctx.closePath();
 		};
--- src/functions/inits/initBackground.js
+++ src/functions/inits/initBackground.js
@@ -5,12 +5,27 @@ import Star from '../../classes/Star';
 export const initBackground = (starsRef) => {
 	let stars = [];
 
-	for (let i = 0; i < 100; i++) {
+	const colors = [
+		'rgb(240, 235, 141)',
+		'rgb(53, 162, 159)',
+		'rgb(210, 51, 105)',
+		'rgb(238, 238, 238)',
+	];
+
+	for (let i = 0; i < 125; i++) {
+		const randomColorsIndex = Math.floor(Math.random() * colors.length);
+
+		const color = `${colors[randomColorsIndex]}`;
+
+		const alpha = (Math.random() * (0.9 - 0.2) + 0.2).toFixed(3);
+		console.log(alpha);
+
 		const star = new Star(
 			Math.floor(Math.random() * CANVAS_WIDTH - 10) + 5,
 			Math.floor(Math.random() * CANVAS_HEIGHT - 10) + 5,
 			1,
-			Math.floor(Math.random() * 8 + 2) / 8
+			color,
+			alpha
 		);
 
 		stars.push(star);
