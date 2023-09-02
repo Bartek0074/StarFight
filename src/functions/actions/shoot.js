5ca90dab62fe8d3e07bb24e9956334a68bb4cf89
Bartek0074 <bartekdo123@gmail.com>
2023-09-02 09:41:16 +0200

    added code to updateMissiles that removing every missile from array when missile is outside the canva

--- src/functions/actions/shoot.js
+++ src/functions/actions/shoot.js
@@ -1,30 +1,20 @@
-import { CANVAS_WIDTH, CANVAS_HEIGHT } from '../../data/variables';
+import { SPACESHIP_WIDTH } from '../../data/variables';
 
 import Missile from '../../classes/Missile';
 
 import { laserShotOneSound } from '../soundEffects/laserShotOneSound';
-
-import { SPACESHIP_WIDTH } from '../../data/variables';
 
 export const shoot = (playerPosition, missilesRef, type = 'single_shot') => {
 	if (type === 'single_shot') {
 		laserShotOneSound();
 	}
 
-	let newMissiles = [...missilesRef.current].filter(
-		(missile) =>
-			missile.position.y > 0 &&
-			missile.position.y < CANVAS_HEIGHT &&
-			missile.position.x > 0 &&
-			missile.position.x < CANVAS_WIDTH
-	);
-
 	const missileRadius = 2;
 	const missileVelocityY = -4;
 	const missileVelocityX = 0;
-	const missileColor= 'rgb(243, 36, 36)';
+	const missileColor = 'rgb(243, 36, 36)';
 
-	newMissiles.push(
+	missilesRef.current.push(
 		new Missile(
 			'player',
 			playerPosition.x + SPACESHIP_WIDTH / 2 - missileRadius / 2,
@@ -35,6 +25,4 @@ export const shoot = (playerPosition, missilesRef, type = 'single_shot') => {
 			missileColor
 		)
 	);
-
-	missilesRef.current = newMissiles;
 };
--- src/functions/updates/updateMissiles.js
+++ src/functions/updates/updateMissiles.js
@@ -1,3 +1,16 @@
+import { CANVAS_WIDTH, CANVAS_HEIGHT } from '../../data/variables';
+
 export const updateMissiles = (ctx, missilesRef) => {
-	missilesRef.current.forEach((missile) => missile.update(ctx));
+	missilesRef.current.forEach((missile, missileIndex) => {
+		if (
+			missile.position.y < -5 ||
+			missile.position.y > CANVAS_HEIGHT + 5 ||
+			missile.position.x < -5 ||
+			missile.position.x > CANVAS_WIDTH + 5
+		) {
+			missilesRef.current.splice(missileIndex, 1);
+			return;
+		}
+		missile.update(ctx);
+	});
 };
